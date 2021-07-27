f3b6c5ab078eacbf06fea33d0fad462662a6fbc0
Oleksandr Korshun <akorshun92@gmail.com>
2021-07-27 13:20:51 +0300

    Registration flow draft

--- features/step_definitions/visit_steps.rb
+++ features/step_definitions/visit_steps.rb
@@ -5,5 +5,41 @@ end
 Then(/^I see website name$/) do
   expect(page).to have_content 'Redmine@testautomate.me'
 
-  sleep 5
+  sleep 2
+end
+
+And(/^I click registration button$/) do
+  click_on 'Register'
+end
+
+Then(/^I see registration page$/) do
+  expect(page).to have_content 'Регистрация'
+end
+
+And(/^I fill in Login name$/) do
+  fill_in 'user_login', with: 'username'
+end
+
+And(/^I fill in password$/) do
+  fill_in 'user_password', with: 'password'
+end
+
+And(/^I fill in Approval$/) do
+  fill_in 'user_password_confirmation', with: 'confirmationsword'
+end
+
+And(/^I fill in First\-Name$/) do
+  fill_in 'user_firstname', with: 'firstname'
+end
+
+And(/^I fill in Second\-Name$/) do
+  fill_in 'user_lastname', with: 'lastname'
+end
+
+And(/^I fill in email$/) do
+  fill_in 'user_mail', with: 'email'
+end
+
+And(/^I click Accept button$/) do
+  click_on 'commit'
 end
--- features/visit.feature
+++ features/visit.feature
@@ -1,5 +1,18 @@
 Feature: Visitor visits Redmine website
 
-  Scenario: Visitor can see the website name
+#  Scenario: Visitor can see the website name
+#    When I open homepage
+#    Then I see website name
+
+  Scenario: I register on the website
     When I open homepage
     Then I see website name
+    And I click registration button
+#    Then I see registration page
+    And I fill in Login name
+    And I fill in password
+    And I fill in Approval
+    And I fill in First-Name
+    And I fill in Second-Name
+    And I fill in email
+    And I click Accept button
